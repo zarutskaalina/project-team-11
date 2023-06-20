86d66dd2fdacefb90b6774a7b7271d36d009a314
zarutskaalina <130370875+zarutskaalina@users.noreply.github.com>
2023-06-20 15:47:58 +0000

    Deploying to gh-pages from @ zarutskaalina/project-team-11@4a07abd209b38b5908caa8582df674c1bd72dd51 🚀

---
+++ index.18dde7e7.js
@@ -0,0 +1,2 @@
+var swiper=new Swiper(".swiper",{direction:"horizontal",loop:!1,pagination:{el:".swiper-pagination",clickable:!0},navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},scrollbar:{el:".swiper-scrollbar"},slidesPerView:"auto",spaceBetween:18});
+//# sourceMappingURL=index.18dde7e7.js.map
