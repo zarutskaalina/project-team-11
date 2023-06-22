2873926cf1226e1e0e583212a38c227922a70417
zarutskaalina <130370875+zarutskaalina@users.noreply.github.com>
2023-06-22 08:06:59 +0000

    Deploying to gh-pages from @ zarutskaalina/project-team-11@d49ef1eab32652a137adbe0560ccff848b46d3f4 🚀

---
+++ index.0ae8b0fe.js
@@ -0,0 +1,2 @@
+!function(){var e={openOrderBtn:document.querySelector("[data-order-open]"),closeOrderBtn:document.querySelector("[data-order-close]"),modal:document.querySelector("[data-order]")};function r(){e.modal.classList.toggle("is-hidden")}e.openOrderBtn.addEventListener("click",r),e.closeOrderBtn.addEventListener("click",r)}();
+//# sourceMappingURL=index.0ae8b0fe.js.map
